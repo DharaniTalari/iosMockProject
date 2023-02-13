cfd6749aaa6071fc051517f04734054dfe1323f7
Talari <talaridharani5@gmail.com>
2023-02-13 18:34:14 +0530

    my first commit

---
+++ DharaniGit_iOS_MockProject/DharaniGit_iOS_MockProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DharaniGit_iOS_MockProject
+//
+//  Created by Talari, Dharani on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
